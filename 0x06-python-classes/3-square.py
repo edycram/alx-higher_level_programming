59adaa067dc1d7246f39337bd19942db9bcce30f
edycram <edycram4u@gmail.com>
2022-10-11 20:47:42 -0700

    Task 3. Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
+""" defines a square """
+
+
 class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with optional size.
-    Public instance method: def area(self).
-    """
+    """ square with private instance attribute size """
 
     def __init__(self, size=0):
-        """Initializes the data."""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        """
+        initializes square
+        Args:
+            size: size of side of square
+        """
+
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be an integer')
 
     def area(self):
-        """Returns the current square area."""
+        """
+        finds area of square
+        Returns:
+            the area of the square
+        """
+
         return self.__size ** 2
